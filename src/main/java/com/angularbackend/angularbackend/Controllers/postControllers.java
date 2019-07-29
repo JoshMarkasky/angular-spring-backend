9e199b901a166acbeee41be7dfc503d96a07df0e
JoshMarkasky <markasky.Josh@gmail.com>
2019-07-28 20:35:32 -0600

    add edit and delete to post contorller

--- src/main/java/com/angularbackend/angularbackend/Controllers/postControllers.java
+++ src/main/java/com/angularbackend/angularbackend/Controllers/postControllers.java
@@ -7,6 +7,7 @@ import com.angularbackend.angularbackend.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.PostUpdate;
 import java.util.Map;
 import java.util.Optional;
 
@@ -34,7 +35,6 @@ public class postControllers {
     @CrossOrigin
     @PostMapping("/postPost")
     public String postPost(@RequestBody Map<String, String>data){
-        System.out.println(data);
         post post = new post();
         Optional<User> user = userRepo.findById((long)1);
 
@@ -45,6 +45,26 @@ public class postControllers {
         postRepo.save(post);
 
         return "it worked!!";
+    }
+
+    @CrossOrigin
+    @PostMapping("/editPost/{id}")
+    public String editPost(@RequestBody Map<String, String>data){
+        post post = new post();
+        post.setTitle(data.get("title"));
+        post.setBody(data.get("body"));
+
+        postRepo.save(post);
+
+        return "it worked";
+    }
+
+    @CrossOrigin
+    @DeleteMapping("/deletePost/{id}")
+    public String deletePost(@PathVariable Long id){
+        postRepo.deleteById(id);
+
+        return "it worked";
     }
 
 }
